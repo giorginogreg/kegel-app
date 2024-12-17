3ab00fe1f04b1eff5e033775164898ee8063236b
ggadt <nydsk2011@gmail.com>
2024-12-17 12:05:23 +0100

    refactor(upgrade): upgraded material to v15

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,7 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import { MatButtonModule } from '@angular/material/button';
+import { MatLegacyButtonModule as MatButtonModule } from '@angular/material/legacy-button';
 import { MatToolbarModule } from '@angular/material/toolbar';
 
 @NgModule({
