d43db5b0fea63bf7d1816f39438003eac9f13ab6
ggadt <nydsk2011@gmail.com>
2024-12-17 13:37:26 +0100

    refactor(upgrade): upgraded ng to v19

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -5,7 +5,8 @@ import {Title} from "@angular/platform-browser";
 @Component({
     selector: 'app-home',
     templateUrl: './home.component.html',
-    styleUrls: ['./home.component.scss']
+    styleUrls: ['./home.component.scss'],
+    standalone: false
 })
 export class HomeComponent implements OnInit {
     starting = new Audio();
