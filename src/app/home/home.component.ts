a6ccfa433c97524ab6fe7a9c46e1297cce5ca1c0
ggadt <nydsk2011@gmail.com>
2024-12-17 11:33:20 +0100

    Moved enum to its folder

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Status } from '../status';
+import { Status } from '../../enums/status';
 import {Title} from "@angular/platform-browser";
 
 @Component({
