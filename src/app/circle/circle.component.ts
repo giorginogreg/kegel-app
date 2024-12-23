f4bc792ba463a7d4ea5f401a452a5b42e3200f39
Gregorio Giorgino <giorgino.greg@gmail.com>
2024-12-23 18:09:51 +0100

    chore: code formatting

--- src/app/circle/circle.component.ts
+++ src/app/circle/circle.component.ts
@@ -9,8 +9,8 @@ import { Status } from 'src/enums/status';
   styleUrl: './circle.component.scss',
 })
 export class CircleComponent {
-  
-  @ViewChild('circleCanvas', { static: true }) canvasRef!: ElementRef<HTMLCanvasElement>;
+  @ViewChild('circleCanvas', { static: true })
+  canvasRef!: ElementRef<HTMLCanvasElement>;
   @ViewChild('duration') durationRef!: ElementRef<any>;
   canvas: HTMLCanvasElement;
   private ctx!: CanvasRenderingContext2D;
@@ -21,21 +21,20 @@ export class CircleComponent {
   canvasXcenter: number;
   canvasYcenter: number;
 
-  private startTime!: number;
-// Stato corrente del raggio
+  // Stato corrente del raggio
   currentRadius: number;
   animationFrame: number;
   isAnimating: boolean;
-constructor() {}
+
+  constructor() {}
 
   ngOnInit() {
     this.canvas = this.canvasRef.nativeElement;
     this.canvasXcenter = this.canvas.width / 2;
     this.canvasYcenter = this.canvas.height / 2;
 
-    this.ctx = this.canvasRef.nativeElement.getContext('2d')!
-     this.currentRadius = this.startRadius;
-
+    this.ctx = this.canvasRef.nativeElement.getContext('2d')!;
+    this.currentRadius = this.startRadius;
   }
 
   private drawCircle(radius: number): void {
@@ -57,47 +56,42 @@ constructor() {}
   }
 
   private animate = (duration: number, increase: boolean): void => {
-    if(this.isAnimating) return
-    const targetRadius = increase ? this.endRadius : this.startRadius
+    if (this.isAnimating) return;
+    const targetRadius = increase ? this.endRadius : this.startRadius;
     const startTime = performance.now();
-      const initialRadius = this.currentRadius;
+    const initialRadius = this.currentRadius;
 
-      console.log("Animate called");
-      
-      const step = (now: number) => {
-        const elapsedTime = now - startTime;
-        const progress = Math.min(elapsedTime / duration, 1);
-        console.log(progress);
+    const step = (now: number) => {
+      const elapsedTime = now - startTime;
+      const progress = Math.min(elapsedTime / duration, 1);
 
-        this.currentRadius = initialRadius + (targetRadius - initialRadius) * progress;
-        this.drawCircle(this.currentRadius);
+      this.currentRadius =
+        initialRadius + (targetRadius - initialRadius) * progress;
+      this.drawCircle(this.currentRadius);
 
-        if (progress < 1) {
-          this.animationFrame = requestAnimationFrame(step);
-        } else this.isAnimating = false;
-      };
+      if (progress < 1) {
+        this.animationFrame = requestAnimationFrame(step);
+      } else this.isAnimating = false;
+    };
 
-      cancelAnimationFrame(this.animationFrame); // Annulla eventuali animazioni precedenti
-      this.animationFrame = requestAnimationFrame(step);
-    
+    cancelAnimationFrame(this.animationFrame); // Annulla eventuali animazioni precedenti
+    this.animationFrame = requestAnimationFrame(step);
   };
 
   decrease() {
-    //this.startAnimation(this.durationRef.nativeElement.value, false);
-    this.animate(this.durationRef.nativeElement.value, false)
+    this.animate(this.durationRef.nativeElement.value, false);
   }
 
   increase() {
-    this.animate(this.durationRef.nativeElement.value, true)
+    this.animate(this.durationRef.nativeElement.value, true);
   }
 
   clear() {
-    cancelAnimationFrame(this.animationFrame)
-    this.isAnimating= false
-    this.currentRadius = this.startRadius
-    this.canvas.width = this.canvas.width
-    this.canvas.height = this.canvas.height
+    cancelAnimationFrame(this.animationFrame);
+    this.isAnimating = false;
+    this.currentRadius = this.startRadius;
+    this.canvas.width = this.canvas.width;
+    this.canvas.height = this.canvas.height;
     this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
   }
-
 }
